1835e04b4e00713e1c05a11cc601f334421459c0
Bruni <juanbruni@live.com>
2021-06-05 21:26:38 -0300

    Create tp4-ej2.py

---
+++ tp4-ej2.py
@@ -0,0 +1,53 @@
+################
+# Juan Ignacio Bruni - @Juanbruni
+# Plantilla de ejercicio
+# UNRN Andina - Introducción a la Ingenieria en Computación
+################
+def prueba():
+    saludo="Suma lenta"
+    saludo_titulo= saludo.upper()
+    print(saludo_titulo+ "\n")
+    
+    numero = int(input("Ingrese el numero A: "))
+    otro_numero= int(input("Ingrese el numero B: "))
+    suma_lenta(numero, otro_numero)
+
+def suma_lenta(numero, otro_numero):
+    suma_en_pantalla=0
+    numero_menor=0
+    resultado_suma=numero+otro_numero
+
+
+    if numero > otro_numero:
+        numero_menor=otro_numero
+        print(f"\nEmpezamos desde:  {numero_menor}")
+    else:
+        numero_menor=numero
+        print(f"\nEmpezamos desde:  {numero_menor}")
+
+        
+    if resultado_suma < 0 and (numero < 0 > otro_numero):
+        while suma_en_pantalla != resultado_suma:
+            suma_en_pantalla = numero_menor - 1
+            print(f"{numero_menor} + (-1)  =  {suma_en_pantalla}")
+            numero_menor=numero_menor-1
+            
+
+            
+    elif (numero == 0 or otro_numero == 0):
+        print(f" {numero_menor} + {otro_numero}  = {suma_en_pantalla}")
+        
+
+        
+    else:
+        while suma_en_pantalla != resultado_suma:
+            suma_en_pantalla = numero_menor + 1
+            print(f"{numero_menor} + 1  = {suma_en_pantalla}")
+            numero_menor=numero_menor+1
+
+
+            
+    print(f"El resultado final es:  {resultado_suma}")
+
+if __name__ == "__main__":
+    prueba()
