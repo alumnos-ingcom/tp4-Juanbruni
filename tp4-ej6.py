59d0db0038d02e8a798796cbd1c02d2509beff66
Bruni <juanbruni@live.com>
2021-06-20 00:07:45 -0300

    Update tp4-ej6.py

--- tp4-ej6.py
+++ tp4-ej6.py
@@ -3,30 +3,43 @@
 # Plantilla de ejercicio
 # UNRN Andina - Introducción a la Ingenieria en Computación
 ################
+
+
+def minimo(lista):
+    for i in range(0, len(lista)):
+        minimo=lista[0]
+        if minimo<=lista[i]:
+            pass
+        else:
+            minimo=lista[i]
+    return minimo
+
+def maximo(lista):
+    for i in range(0, len(lista)):
+        maximo=lista[0]
+        if maximo>=lista[i]:
+            pass
+        else:
+            maximo=lista[i]
+    return maximo
+    
 def prueba():
     saludo="Máximo / Mínimo "
     saludo_titulo= saludo.upper()
     print(saludo_titulo+"\n")
     lista=[]
     ingreso=0
-    
     while ingreso != "":
-        ingreso=input("Ingrese un numero: ")
+        ingreso=input("Ingrese numero: ")
         lista.append(ingreso)
     else:
         lista.pop()
-        lista.sort()
         minimo(lista)
         maximo(lista)
-
-def minimo(lista):
-    minimo=lista[0]
-    print(f"Minimo es: {minimo}")
-
-def maximo(lista):
-    lista.reverse()
-    maximo=lista[0]
-    print(f"Máximo es: {maximo}")
+        numero_minimo=minimo(lista)
+        numero_maximo=maximo(lista)
+        print(f"Minimo es: {numero_minimo}")
+        print(f"Máximo es: {numero_maximo}")
     
 if __name__ == "__main__":
     prueba()
