40ee1f386e84ac56bbd94b43c14c2e69fea0cca3
Bruni <juanbruni@live.com>
2021-06-19 23:52:28 -0300

    Update tp4-ej4.py

--- tp4-ej4.py
+++ tp4-ej4.py
@@ -3,21 +3,24 @@
 # Plantilla de ejercicio
 # UNRN Andina - Introducción a la Ingenieria en Computación
 ################
+
+
+def compara(numero, otro_numero):
+    if numero <  otro_numero:
+        return -1
+    elif (numero == otro_numero):
+        return 0
+    else:
+        return 1
 def prueba():
     saludo="Comparación de números"
     saludo_titulo= saludo.upper()
     print(saludo_titulo+ "\n")
+    
     numero=int(input("Ingrese un número: "))
     otro_numero=int(input("Ingrese otro número: "))
     compara(numero, otro_numero)
-
-def compara(numero, otro_numero):
-    if numero <  otro_numero:
-        print("\n-1")
-    elif (numero == otro_numero):
-        print("\n0")
-    else:
-        print("\n1")
-    
+    resultado=compara(numero, otro_numero)
+    print (resultado)
 if __name__ == "__main__":
     prueba()
